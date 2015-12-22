e18b05f58253636c272c793f0282591b4c5e8669
Glur4k <tonon.san@gmail.com>
2015-12-22 11:41:21 +0100

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name multiNavigationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the multiNavigationApp
+ */
+angular.module('multiNavigationApp')
+  .controller('MainCtrl', function () {
+
+  });
